de194c2dfb6d449f2e5cb329679a0628865c530a
coolcrazydog <fan.gao1989@gmail.com>
2019-03-02 13:53:58 -0500

    show recipe details on selected from list(use Output/Input)

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'Simply a test', 'https://www.cbc.ca/food/content/images/recipes/CowboySteak.jpg'),
-    new Recipe('A Test Recipe', 'Simply a test', 'https://www.cbc.ca/food/content/images/recipes/CowboySteak.jpg'),
-new Recipe('A Test Recipe', 'Simply a test', 'https://www.cbc.ca/food/content/images/recipes/CowboySteak.jpg')
+    new Recipe('Test Recipe 1', 'recipe 1 description', 'https://www.cbc.ca/food/content/images/recipes/CowboySteak.jpg'),
+    new Recipe('Test Recipe 2', 'recipe 2 description', 'https://www.cbc.ca/food/content/images/recipes/CowboySteak.jpg'),
+new Recipe('Test Recipe 3', 'recipe 3 description', 'https://www.cbc.ca/food/content/images/recipes/CowboySteak.jpg')
 ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeItemSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
