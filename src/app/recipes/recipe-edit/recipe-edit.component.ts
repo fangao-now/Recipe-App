0840100365def009f063d8ca0f6d02be7d158f6a
coolcrazydog <fan.gao1989@gmail.com>
2019-03-11 21:24:27 -0400

    implement cancel button and delete in recipe details

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
@@ -14,7 +14,8 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService,
+    private router: Router) { }
 
   ngOnInit() {
     // this observable is managed by angular, thus we don't need to clean up this in ngOnDestroy()
@@ -68,6 +69,7 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(newRecipe);
     }
+    this.onCancel();
   }
 
   getIngredientsControls() {
@@ -82,5 +84,9 @@ export class RecipeEditComponent implements OnInit {
       })
     );
     console.log(this.recipeForm.value.ingredients);
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 }
--- src/app/recipes/recipes-details/recipes-details.component.ts
+++ src/app/recipes/recipes-details/recipes-details.component.ts
@@ -33,4 +33,9 @@ export class RecipesDetailsComponent implements OnInit {
     // this.router.nagivate(['../', this.id, 'edit'], {relativeTo: this.route});
     this.router.navigate(['edit'], {relativeTo: this.route});
   }
+
+  onDelete() {
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['/recipes']);
+  }
 }
