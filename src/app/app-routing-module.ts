0c9356c3aa0a49668a2c91346bd9c0c6c43b7399
coolcrazydog <fan.gao1989@gmail.com>
2019-03-14 19:42:45 -0400

    added simple home page for the app

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full' }
+    { path: '', component: HomeComponent}
 ];
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
